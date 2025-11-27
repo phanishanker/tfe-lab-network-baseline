a9f80c4f4606451dce7ca3e412e6c5398d0e0830
phanishanker <phanishanker@gmail.com>
2025-11-27 23:56:31 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,25 +7,25 @@ locals {
   default_tags = {
     Owner       = "platform-team"
     CostCenter  = "CLOUD-PLATFORM"
-    Environment = "lab"
+    Environment = var.environment
     ManagedBy   = "Terraform-Enterprise"
     Project     = "tfe-lab-1"
   }
 }
 
-resource "random_id" "suffix" {
-  byte_length = 4
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
 }
 
 resource "aws_s3_bucket" "secure_bucket" {
-  bucket = "acme-tfe-lab1-${var.aws_region}-${random_id.suffix.hex}"
+  bucket = "acme-${var.environment}-${random_string.suffix.result}"
   tags   = local.default_tags
 }
+
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.secure_bucket.id
-
   versioning_configuration {
     status = "Enabled"
   }
 }
-//
