e3bd11f7407f647017c8cfaa4b3de3186b89243f
phanishanker <phanishanker@gmail.com>
2025-11-27 17:38:48 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+locals {
+  default_tags = {
+    Owner       = "platform-team"
+    CostCenter  = "CLOUD-PLATFORM"
+    Environment = "lab"
+    ManagedBy   = "Terraform-Enterprise"
+    Project     = "tfe-lab-1"
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "secure_bucket" {
+  bucket = "acme-tfe-lab1-${var.aws_region}-${random_id.suffix.hex}"
+  tags   = local.default_tags
+}
