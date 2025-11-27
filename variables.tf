7cd0fdb01a398346ee6e0032d3de96fdbe21d49a
phanishanker <phanishanker@gmail.com>
2025-11-28 00:23:29 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
+variable "environment" {
+  description = "Workspace environment (dev or prod)"
+  type        = string
+}
+
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-southeast-1"
 }
+
