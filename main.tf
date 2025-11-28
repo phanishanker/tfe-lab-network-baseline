a14af0a3ec5db7ce5aba1763742b3fb266f9d757
phanishanker <phanishanker@gmail.com>
2025-11-28 15:00:29 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,45 @@
-locals {
-  default_tags = {
-    Owner       = "platform-team"
-    CostCenter  = "CLOUD-PLATFORM"
-    Environment = var.environment
-    ManagedBy   = "Terraform-Enterprise"
-    Project     = "tfe-lab-1"
-  }
+# Block public access entirely
+resource "aws_s3_bucket_public_access_block" "secure_block" {
+  bucket                  = aws_s3_bucket.secure_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
-resource "aws_s3_bucket" "secure_bucket" {
-  bucket = "acme-${var.environment}-${random_string.suffix.result}"
-  tags   = local.default_tags
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
+# Default encryption (AES-256)
+resource "aws_s3_bucket_server_side_encryption_configuration" "secure_encrypt" {
   bucket = aws_s3_bucket.secure_bucket.id
-  versioning_configuration {
-    status = "Enabled"
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
+}
+
+# Enforce HTTPS only access (block HTTP)
+resource "aws_s3_bucket_policy" "https_only" {
+  bucket = aws_s3_bucket.secure_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid:       "EnforceTLS",
+        Effect:    "Deny",
+        Principal: "*",
+        Action:    "s3:*",
+        Resource: [
+          aws_s3_bucket.secure_bucket.arn,
+          "${aws_s3_bucket.secure_bucket.arn}/*"
+        ],
+        Condition: {
+          Bool: {
+            "aws:SecureTransport": "false"
+          }
+        }
+      }
+    ]
+  })
 }
